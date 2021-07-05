3b7437cf9f7ea4647513bcb14fa690eaac913333
Tamir Hen <51208520+TamirHen@users.noreply.github.com>
2021-07-05 15:06:27 -0700

    Mobile navbar animation finished

--- components/Menus.js
+++ components/Menus.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "../styles/Menus.module.scss";
 import Logo from "../assets/logo.svg";
 import Hamburger from "../assets/hamburger.svg";
@@ -6,11 +6,13 @@ import LinkedinIcon from "../assets/linkedin.svg";
 import InstagramIcon from "../assets/instagram.svg";
 import TwitterIcon from "../assets/twitter.svg";
 import SideMenuIcon from "../assets/sideMenuIcon.svg";
+import Fade from "react-reveal";
 
 const Menus = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={styles.menusWrapper}>
-            <Hamburger className={styles.hamburger}/>
+            <Hamburger className={styles.hamburger} onClick={() => setIsOpen(true)}/>
             <div className={styles.sideMenuWrapper}>
                 <nav className={styles.sideMenu}>
                     <a href="/"><Logo className={styles.logo}/></a>
@@ -22,10 +24,13 @@ const Menus = () => {
                     <SideMenuIcon className={styles.sideMenuIcon}/>
                 </nav>
             </div>
-            <nav className={styles.mobileNav}>
-                <button>ABOUT</button>
-                <button>OUR WORK</button>
-                <button>CONTACT US</button>
+            <nav className={styles.mobileNav} style={{maxWidth: isOpen ? '100%' : '0'}}>
+                <button className={styles.buttonX} onClick={() => setIsOpen(false)}>x</button>
+                <div className={styles.buttonsWrapper}>
+                    <button style={{opacity: isOpen ? '100%' : '0'}}>ABOUT</button>
+                    <button style={{opacity: isOpen ? '100%' : '0'}}>OUR WORK</button>
+                    <button style={{opacity: isOpen ? '100%' : '0'}}>CONTACT US</button>
+                </div>
             </nav>
         </div>
     );
