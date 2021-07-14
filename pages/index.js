80c311166a77f6b5b10e7b681427e56f59a5b3ff
Tamir Hen <51208520+TamirHen@users.noreply.github.com>
2021-07-14 15:28:41 -0700

    Icon dimensions fixed + different sizes icons added

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,13 @@ export default function Home({workerPressedHandler, aboutSection, ourWorkSection
         <div className={styles.main}>
             <Head>
                 <title>Palladom Solutions | Web Design & Development Company</title>
-                <meta name="description" content="Website design and development agency in Vancouver, Canada. Full-service digital web design & development, affordable best prices with SEO and Ecommerce for your business."/>
+                <meta name="description"
+                      content="Website design and development agency in Vancouver, Canada. Full-service digital web design & development, affordable best prices with SEO and Ecommerce for your business."/>
                 <link rel="icon" href="/favicon.ico"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
             </Head>
 
             <div className={styles.textCycleWrapper}><TextCycleIcon className={styles.textCycle}/></div>
